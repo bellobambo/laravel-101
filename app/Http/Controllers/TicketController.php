5267acd1c8748791096a61642dabd688333b02d1
bellobambo <bellobambo21@gmail.com>
2023-12-26 15:11:41 +0100

    mvc

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -43,16 +43,8 @@ class TicketController extends Controller
 
         if ($request->file('attachment')) {
 
-            $ext = ($request->file('attachment')->extension());
+            $this->storeAttachment($request, $ticket);
 
-            $contents = file_get_contents($request->file('attachment'));
-
-            $filename = Str::random(25);
-
-            $path = "attachments/$filename.$ext";
-
-            Storage::disk('public')->put($path, $contents);
-            $ticket->update(['attachment' => $path]);
         }
 
 
@@ -74,7 +66,8 @@ class TicketController extends Controller
      */
     public function edit(Ticket $ticket)
     {
-        //
+        return view('ticket.edit', compact('ticket'));
+
     }
 
     /**
@@ -82,7 +75,14 @@ class TicketController extends Controller
      */
     public function update(UpdateTicketRequest $request, Ticket $ticket)
     {
-        //
+        $ticket->update(['title' => $request->title, 'description' => $request->description]);
+        if ($request->file('attachment')) {
+            Storage::disk('public')->delete($request, $ticket);
+
+            $this->storeAttachment($request, $ticket);
+
+        }
+        return redirect(route('ticket.index'));
     }
 
     /**
@@ -90,6 +90,21 @@ class TicketController extends Controller
      */
     public function destroy(Ticket $ticket)
     {
-        //
+        $ticket->delete();
+        return redirect(route('ticket.index'));
+    }
+
+    protected function storeAttachment($request, $ticket)
+    {
+        $ext = ($request->file('attachment')->extension());
+
+        $contents = file_get_contents($request->file('attachment'));
+
+        $filename = Str::random(25);
+
+        $path = "attachments/$filename.$ext";
+
+        Storage::disk('public')->put($path, $contents);
+        $ticket->update(['attachment' => $path]);
     }
 }
--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -2,4 +2,4 @@
     'class' =>
         'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm',
 ]) !!}
-    class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full">{{$default}}</textarea>
+    class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full">{{$value}}</textarea>
